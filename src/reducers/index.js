55a5d3e6182751e2de2d4b15dc26e57f3985a320
Neil Halligan <neilhalligan2972@gmail.com>
2018-08-03 10:14:15 +0100

    first commit

--- src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-/**
- * Exports of this module is an object where the keys are the names of the properties the reducers operate on,
- * and the value is the actual reducer
- */
-export { questions } from './questions';
